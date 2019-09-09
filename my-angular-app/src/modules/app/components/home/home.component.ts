c729f639dde029f812dadf973799a7ea9ee85e8d
EstevenJaviier <estirastam.c@gmail.com>
2019-09-09 16:42:23 +0000

    second commit

---
+++ my-angular-app/src/modules/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ns-items",
+    templateUrl: "./home.component.html"
+})
+
+export class Home implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+
+    }
+}
