ea0cdde58cd42a228350f699b510d95b143e8255
EstevenJaviier <estirastam.c@gmail.com>
2019-09-09 18:27:38 +0000

    Aun sin enviar sms

--- my-angular-app/src/modules/app/app.module.ts
+++ my-angular-app/src/modules/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./components/home/home.component";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
-// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+ import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
     bootstrap: [
@@ -18,12 +18,13 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        HomeComponent
     ],
     providers: [],
     schemas: [
--- my-angular-app/src/modules/app/components/home/home.component.ts
+++ my-angular-app/src/modules/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "ns-items",
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ['./home.component.css']
 })
 
-export class Home implements OnInit {
+export class HomeComponent implements OnInit {
 
     constructor() { }
 
