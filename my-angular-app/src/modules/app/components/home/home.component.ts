3601e90227bd472e20df15e29db0d4fc3dad6e5c
EstevenJaviier <estirastam.c@gmail.com>
2019-09-11 16:23:15 +0000

    Probando TNSPhone

--- my-angular-app/src/modules/app/components/home/home.component.ts
+++ my-angular-app/src/modules/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import * as TNSPhone from 'nativescript-phone';
 
 @Component({
     selector: "ns-items",
@@ -8,9 +9,30 @@ import { Component, OnInit } from "@angular/core";
 
 export class HomeComponent implements OnInit {
     constructor() {
-
+        this.callHome();
+        this.messageParents();
      }
 
     ngOnInit(): void {
+
     }
+
+    /// Dial a phone number.
+    public callHome() {
+    const phoneNumber = '3205157279';
+    TNSPhone.requestCallPermission('You should accept the permission to be able to make a direct phone call.')
+        .then(() => TNSPhone.dial(phoneNumber, false))
+        .catch(() => TNSPhone.dial(phoneNumber, true));
+    }
+
+    // Text a number (or multiple numbers)
+    public messageParents() {
+        TNSPhone.sms(['3205157279'], "Text till your fingers bleed")
+            .then((args) => {
+                console.log(JSON.stringify(args));
+            }, (err) => {
+                console.log('Error: ' + err);
+            })
+    }
+
 }
